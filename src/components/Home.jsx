778cd64886a81eb1ca03e60965e34219b8076c4a
Harshvardhan <shreeharshpb11@gmail.com>
2024-01-31 22:53:45 +0530

    create home functionality

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+const Home = () => {
+  return (
+    <div className="home_container">
+      <div className="home_info_container">
+        <span>Welcome, we are a cool</span>
+        <h1>Game Development Studio</h1>
+      </div>
+      <div className="home_box_container">
+        <div className="box">
+            <span>_01</span>
+            <h3>Full Cycle Game Developement</h3>
+        </div>
+        <div className="box">
+            <span>_02</span>
+            <h3>Nft Game Development</h3>
+        </div>
+        <div className="box">
+            <span>_03</span>
+            <h3>Game Testing & Porting</h3>
+        </div>
+        <div className="box">
+            <span>_04</span>
+            <h3>Gamification Services</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

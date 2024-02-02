a464a5bff4518cc16947a989b700d82d0fe5a696
Harshvardhan <shreeharshpb11@gmail.com>
2024-02-02 18:43:39 +0530

    created api for card

---
+++ src/utils/api.js
@@ -0,0 +1,22 @@
+export const acordion = [
+    {
+        id:1,
+        name:'PC',
+        desciption:'Curabitur vel ex efficitur, posuere risus et, consequat metus. Phasellus facilisis quis ligula nec feugiat. Ut fringilla, risus sed tristique maximus, orci augue tristique nunc, sit amet ultrices nibh elit ut magna. Curabitur ornare lacus in augue placerat dapibus sit amet vel dolor.'
+    },
+    {
+        id:2,
+        name:'CONSOLE',
+        desciption:'Curabitur vel ex efficitur, posuere risus et, consequat metus. Phasellus facilisis quis ligula nec feugiat. Ut fringilla, risus sed tristique maximus, orci augue tristique nunc, sit amet ultrices nibh elit ut magna. Curabitur ornare lacus in augue placerat dapibus sit amet vel dolor.'
+    },
+    {
+        id:3,
+        name:'MOBILE',
+        desciption:'Curabitur vel ex efficitur, posuere risus et, consequat metus. Phasellus facilisis quis ligula nec feugiat. Ut fringilla, risus sed tristique maximus, orci augue tristique nunc, sit amet ultrices nibh elit ut magna. Curabitur ornare lacus in augue placerat dapibus sit amet vel dolor.'
+    },
+    {
+        id:4,
+        name:'AR/VR',
+        desciption:'Curabitur vel ex efficitur, posuere risus et, consequat metus. Phasellus facilisis quis ligula nec feugiat. Ut fringilla, risus sed tristique maximus, orci augue tristique nunc, sit amet ultrices nibh elit ut magna. Curabitur ornare lacus in augue placerat dapibus sit amet vel dolor.'
+    },
+]
